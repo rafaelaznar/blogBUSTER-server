008c997a5febc12332f9ba937006d1cc4471c2fa
Rafael Aznar <rafaaznar@gmail.com>
2019-11-05 09:06:10 +0100

    fechas

--- src/main/java/net/ausiasmarch/dao/PostDao.java
+++ src/main/java/net/ausiasmarch/dao/PostDao.java
@@ -1,12 +1,10 @@
 package net.ausiasmarch.dao;
 
 import java.sql.Connection;
-import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.List;
 import net.ausiasmarch.bean.BeanInterface;
